3e075edff740aa52721495534ff3443c5a0dda5f
Jake Foronda <jakesforonda@gmail.com>
2025-01-18 13:47:31 -0800

    Made a loop to access all data files

--- scripts/dataprocessing.R
+++ scripts/dataprocessing.R
@@ -1,14 +1,23 @@
-# Need to install the readr package to access RNA Seq TSV files
+# Need to install the readr package to read RNA Seq TSV files
 install.packages("readr")
 
 # Load installed readr package
 library(readr)
 
-# Use file indexing to better access files
+# Make a list of files to access for iterating
 files <- list.files(path = "test/", pattern = "*.tsv", full.names = TRUE)
 
-# Read file into data frame
-df1 <- read_tsv(files[1])
 
-# Keep only gene_id, unstranded, and tpm_unstranded columns
-df1 <- df1[, c("gene_name", "unstranded", "tpm_unstranded")]
+# Make an empty dataframe for merging
+merged_df <- data.frame()
+
+# Read every file into a data frame
+for (file in files) {
+  df <- read_tsv(file)
+
+  # Keep only gene_name and tpm_unstranded columns to use EPIC on
+  df1 <- df[, c("gene_name", "tpm_unstranded")]
+  print(df1)
+  merged_df <- merge(df1, merged_df, by = "gene_name", all = TRUE)
+}
+print(merged_df)
