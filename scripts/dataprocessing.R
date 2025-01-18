1c446ad5ca397425f173d97227c4e1ac75883d36
Jake Foronda <jakesforonda@gmail.com>
2025-01-18 14:47:20 -0800

    Completed processing script to generated merged data

--- scripts/dataprocessing.R
+++ scripts/dataprocessing.R
@@ -7,22 +7,41 @@ library(readr)
 # Make a list of files to access for iterating
 files <- list.files(path = "test/", pattern = "*.tsv", full.names = TRUE)
 
-# Install dplyr package to help manipulate data structures
+# Install packages to help manipulate data structures
 install.packages("dplyr")
+install.packages("tidyr")
+install.packages("purrr")
 
-# Load installed dplyr package
-library(dyplr)
+# Load installed packages
+library(dplyr)
+library(tidyr)
+library(purrr)
 
-# Make an empty dataframe for merging
-merged_df <- data.frame()
+# Make an empty list to collect data frames
+df_list <- list()
 
 # Read every file into a data frame
 for (file in files) {
-  df <- read_tsv(file)
+
+  # Take only sample name from filename
+  sample_name <- sub("\\..*", "", basename(file))
+
+  # Read data
+  data <- read_tsv(file)
 
   # Keep only gene_name and tpm_unstranded columns to use EPIC on
-  df1 <- df[, c("gene_name", "tpm_unstranded")]
-  print(df1)
-  merged_df <- merge(df1, merged_df, by = "gene_name", all = TRUE)
+  df <- data %>%
+    select(gene_name, tpm_unstranded) %>%
+    rename(!!sample_name := tpm_unstranded)
+
+  # Store in list
+  df_list[[sample_name]] <- df
 }
-print(merged_df)
+
+# Merge the collected data frames of each sample
+merged_df <- reduce(df_list, full_join, by = "gene_name")
+
+# Save the merged data into results folder
+output_folder <- "results"
+output_file <- file.path(output_folder, "merged_gene_counts.csv")
+write.csv(merged_df, output_file, row.names = FALSE)
