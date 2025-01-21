eda38fbdcb80928448bfe16c3fb1ee8422ce474a
Jake Foronda <jakesforonda@gmail.com>
2025-01-20 16:03:54 -0800

    Streamlined processing and infiltrate scripts, generated output data

--- scripts/dataprocessing.R
+++ scripts/dataprocessing.R
@@ -1,12 +1,6 @@
 # Need to install the readr package to read RNA Seq TSV files
 install.packages("readr")
 
-# Load installed readr package
-library(readr)
-
-# Make a list of files to access for iterating
-files <- list.files(path = "test/", pattern = "*.tsv", full.names = TRUE)
-
 # Install packages to help manipulate data structures
 install.packages("dplyr")
 install.packages("tidyr")
@@ -16,36 +10,78 @@ install.packages("purrr")
 library(dplyr)
 library(tidyr)
 library(purrr)
+library(readr)
 
-# Make an empty list to collect data frames
-df_list <- list()
+# Make a list of files to access for iterating
+files <- list.files(path = "test/", pattern = "*.tsv", full.names = TRUE)
 
-# Read every file into a data frame
+# Make empty lists to collect tibbles for analyses
+de_list <- list()
+infil_list <- list()
+
+# Read every file and process columns for analyses
 for (file in files) {
 
   # Take only sample name from filename
   sample_name <- sub("\\..*", "", basename(file))
 
-  # Read data
+  # Read file as a tibble
   data <- read_tsv(file)
 
   # Keep only gene_name and tpm_unstranded columns for infiltrate
-  df <- data %>%
+  infil_tbl <- data %>%
 
     # Remove duplicate values in gene_names
     distinct(gene_name, .keep_all = TRUE) %>%
+
+    # Collect gene_name and tpm_usntranded columns
     select(gene_name, tpm_unstranded) %>%
+
+    # Rename the column name as the sample name
     rename(!!sample_name := tpm_unstranded)
 
-  # Store dataframe in list
-  df_list[[sample_name]] <- df
+  # Keep only gene_name and tpm_unstranded columns for DE
+  de_tbl <- data %>%
+
+    # Remove duplicate values in gene_names
+    distinct(gene_name, .keep_all = TRUE) %>%
+
+    # Collect gene_name and usntranded columns
+    select(gene_name, unstranded) %>%
+
+    # Rename the column name as the sample name
+    rename(!!sample_name := unstranded)
+
+  # Store tibbles in list
+  infil_list[[sample_name]] <- infil_tbl
+  de_list[[sample_name]] <- de_tbl
 }
 
-# Merge the collected data frames of each sample
-merged_df <- reduce(df_list, full_join, by = "gene_name")
+# Merge the collected tibbles of each sample
+merged_de_tbl <- reduce(de_list, full_join, by = "gene_name")
+merged_infil_tbl <- reduce(infil_list, full_join, by = "gene_name")
 
+# Preserve gene names to use as row names in matrices
+gene_names <- merged_de_tbl$gene_name
+
+# Remove gene_name column to prep data for conversion
+merged_de_tbl <- merged_de_tbl[, -1]
+merged_infil_tbl <- merged_infil_tbl[, -1]
+
+# Convert tibbles into numeric matrices
+merged_de_matrix <- apply(as.matrix(merged_de_tbl), 2, as.numeric)
+merged_infil_matrix <- apply(as.matrix(merged_infil_tbl), 2, as.numeric)
+
+# Preserve gene_names as row names in matrices
+rownames(merged_de_matrix) <- gene_names
+rownames(merged_infil_matrix) <- gene_names
 
 # Save the merged data into results folder
-output_folder <- "results"
-output_file <- file.path(output_folder, "merged_gene_counts.csv")
-write.csv(merged_df, output_file, row.names = FALSE)
+
+# Label file paths for output
+output_file1 <- file.path("results", "merged_tpm_counts.csv")
+output_file2 <- file.path("results", "merged_unstranded_counts.csv")
+
+# Write processed data for output
+write.csv(merged_de_matrix, output_file1, row.names = TRUE)
+write.csv(merged_infil_matrix, output_file2, row.names = TRUE)
--- scripts/infiltrate.R
+++ scripts/infiltrate.R
@@ -1,7 +1,37 @@
-# Install EPIC
+# Install MCPcounter for immune deconvolution per GitHub instructions
+install.packages(c("devtools", "curl"))
+library(devtools)
+install_github("ebecht/MCPcounter", ref = "master", subdir = "Source")
 
+# Load installed MCPcounter package
+library(MCPcounter)
 
-# Load in merged_data
-merged_path <- file.path("results", "merged_gene_counts.csv")
-data <- read.csv(merged_path)
-print(data)
+# Load the merged data
+data_path <- file.path("results", "merged_tpm_counts.csv")
+merged_data <- read.csv(
+  data_path, header = TRUE, row.names = 1, check.names = FALSE
+)
+
+# Input the matrix with MCPcounter
+results <- MCPcounter.estimate(merged_data, featuresType = "HUGO_symbols")
+
+# Output the results
+output_file <- file.path("results", "infil_counts.csv")
+write.csv(results, output_file, row.names = TRUE)
+
+# Install pheatmap to being analyzing results
+install.packages("pheatmap")
+
+# Load installed pheatmap package
+library(pheatmap)
+
+# Generate png for heatmap
+output_file <- file.path("results", "infil_heatmap.png")
+png(output_file, width = 800, height = 600)
+
+# Plot the heatmap
+pheatmap(as.matrix(results), cluster_rows = TRUE, cluster_cols = TRUE,
+         main = "MCPcounter Cell Type Estimates")
+
+# Close png file
+dev.off()
