8dcb6e522260e299be794782f17c1a66dccc24f5
Jake Foronda <jakesforonda@gmail.com>
2025-01-29 21:22:52 -0800

    Refactor data processing script to improve file handling and metadata integration

--- scripts/dataprocessing.R
+++ scripts/dataprocessing.R
@@ -1,63 +1,55 @@
-# Load installed package
+# Load data.table package
 library(data.table)
 
-# Make a list of files to access for iterating
-files <- list.files(path = "data/", pattern = "*.tsv", full.names = TRUE)
+# Make a list of files to access for iterating -> remove head() after testing
+files <- head(list.files(path = "data/", pattern = "*.tsv", full.names = FALSE))
+
+# Read in metadata file
+metadata <- fread("metadata/gdc_sample_sheet.2025-01-29.tsv")
+metadata <- metadata[, c("File Name", "Case ID")]
+
 
 # Make empty lists to collect data.tables for analyses
 de_list <- list()
 infil_list <- list()
 
+
 print("Processing TSV Files...")
 
 # Read every file and process columns for analyses
 for (file in files) {
-
-  # Take only sample name from filename
-  sample_name <- sub("\\..*", "", basename(file))
+  # Full path to file
+  full_path <- paste("data/", file, sep = "")
 
   # Read file as a data.table
-  data <- fread(file)
+  data <- fread(full_path)
 
   # Only keep rows that have protein_coding genes
   data <- data[gene_type == "protein_coding"]
 
   # Process infiltrate data
   infil_tbl <- data[, .(gene_name, tpm_unstranded)]
-
-  # Clean the gene names by removing transcript versions
   infil_tbl[, gene_name := sub("\\..*", "", gene_name)]
-
-  # Remove duplicate genes
-  infil_tbl <- infil_tbl[!duplicated(gene_name)]
-
-  # Set count column to sample name
-  setnames(infil_tbl, "tpm_unstranded", sample_name)
+  infil_tbl <- infil_tbl[
+    , .(tpm_unstranded = sum(tpm_unstranded)),
+    by = gene_name
+  ]
+  setnames(infil_tbl, "tpm_unstranded", file)
 
   # Process DE data
   de_tbl <- data[, .(gene_name, unstranded)]
-
-  # Clean the gene names by removing transcript versions
   de_tbl[, gene_name := sub("\\..*", "", gene_name)]
-
-  # Remove duplicate genes
-  de_tbl <- de_tbl[!duplicated(gene_name)]
-
-  # Set count column to sample name
-  setnames(de_tbl, "unstranded", sample_name)
+  de_tbl <- de_tbl[, .(unstranded = sum(unstranded)), by = gene_name]
+  setnames(de_tbl, "unstranded", file)
 
   # Store data.tables in lists
-  infil_list[[sample_name]] <- infil_tbl
-  de_list[[sample_name]] <- de_tbl
+  infil_list[[file]] <- infil_tbl
+  de_list[[file]] <- de_tbl
 }
-
-# Sort the data.tables by gene_name to speed up merging
-setorder(de_list[[1]], gene_name)
-setorder(infil_list[[1]], gene_name)
 
 print("Merging Data...")
 
-# Use Reduce to merge data.tables efficiently
+# Use Reduce to merge data.tables
 merged_de_tbl <- Reduce(
   function(x, y) merge(x, y, by = "gene_name", all = TRUE), de_list
 )
@@ -79,6 +71,16 @@ merged_infil_matrix <- as.matrix(merged_infil_tbl)
 # Preserve gene_names as row names in matrices
 rownames(merged_de_matrix) <- gene_names
 rownames(merged_infil_matrix) <- gene_names
+
+# Replace filenames with metadata Case_ID
+de_colnames <- metadata[
+  match(colnames(merged_de_matrix), metadata$"File Name"), "Case ID"
+]
+infil_colnames <- metadata[
+  match(colnames(merged_infil_matrix), metadata$"File Name"), "Case ID"
+]
+colnames(merged_de_matrix) <- de_colnames$"Case ID"
+colnames(merged_infil_matrix) <- infil_colnames$"Case ID"
 
 print("Writing Merged Data...")
 
